f51d6eb44b4314bf5d123238bea71c5aa4f0c239
Autumn Coleman <autumn.eve.coleman@gmail.com>
2016-12-05 11:43:20 -0500

    Ex1

--- print_file.js
+++ print_file.js
@@ -1,6 +1,11 @@
+var file = process.argv[2];
 var fs = require('fs');
 
-var buffer = fs.readFileSync('file1.txt');
-var contents = buffer.toString();
-var contentsCaps = contents.toUpperCase();
-console.log(contentsCaps);
+try {
+  var buffer = fs.readFileSync(file);
+  var contents = buffer.toString();
+  var contentsCaps = contents.toUpperCase();
+  console.log(contentsCaps);
+} catch (error) {
+  console.log('no such file or directory, open', file);
+}
