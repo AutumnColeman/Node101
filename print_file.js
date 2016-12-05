18725e88c1846e8c0f65d8b9179b8f51aa7bafc1
Autumn Coleman <autumn.eve.coleman@gmail.com>
2016-12-05 11:48:27 -0500

    Async ver

--- print_file.js
+++ print_file.js
@@ -1,11 +1,24 @@
 var file = process.argv[2];
 var fs = require('fs');
+//sync version
+// try {
+//   var buffer = fs.readFileSync(file);
+//   var contents = buffer.toString();
+//   var contentsCaps = contents.toUpperCase();
+//   console.log(contentsCaps);
+// } catch (error) {
+//   console.log('no such file or directory, open', file);
+// }
 
-try {
+//async version
+
+fs.readFile(file, function(err, buffer) {
+  if (err) {
+    console.log('no such file or directory, open', file);
+    return;
+  }
   var buffer = fs.readFileSync(file);
   var contents = buffer.toString();
   var contentsCaps = contents.toUpperCase();
   console.log(contentsCaps);
-} catch (error) {
-  console.log('no such file or directory, open', file);
-}
+});
